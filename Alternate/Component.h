5d8b036ee6025019de04cae6ada2019f5a893eee
Albion Halilaj <albha882@student.liu.se>
2022-12-12 17:19:06 +0100

    Doxyfile added and comments fixed for Collides, component needs a comment and the rest is not fixed yet.

--- Alternate/Component.h
+++ Alternate/Component.h
@@ -10,25 +10,22 @@
 
 class Entity;
 class World;
+/**
+ *
+ */
 
 class Component {
 public:
-    /*
-     * Component interface. All virtual methods/destructor
-     * */
     virtual ~Component() = default;
 
-    /*
-     * Why do we need this? what does it do?
-     * */
     virtual void added(Entity &e);
 
-    /*
+  /**
      * update the activity of components
      * */
     virtual bool update(sf::Time dt, Entity &entity, World &world);
 
-    /*
+  /**
      * Render these functionalities
      * */
     virtual void render(sf::RenderWindow &window, Entity &entity);
